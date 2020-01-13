4801426187099fa6ed55b2092cce1a22738470bd
venkat090993 <45332960+venkat090993@users.noreply.github.com>
2020-01-13 23:46:41 +0530

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import SEO from "../components/seo"
 import HeaderBlock from "../components/HeaderBlock"
 import About from "../components/About"
