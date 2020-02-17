9c675aa250ec8b993515da68e1eabbddc1fec874
venkat090993 <45332960+venkat090993@users.noreply.github.com>
2020-02-18 00:53:35 +0530

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Thousand Lights
-
-The documentation is avaialble at [http://thousand-lights.surge.sh/](http://thousand-lights.surge.sh/)
-Website of thousand lights
---
+++ styles-8acb61508ad505685f60.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{Imr2:function(n,o,i){},PHiE:function(n,o,i){},TCpR:function(n,o,i){},dJPS:function(n,o,i){},dXeN:function(n,o,i){},xCva:function(n,o,i){}}]);
+//# sourceMappingURL=styles-8acb61508ad505685f60.js.map
